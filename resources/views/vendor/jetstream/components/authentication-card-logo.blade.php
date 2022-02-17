eea68fb9af030e918c39796ddb7e867b71f7aa26
Joel Rocha <rochab@gmail.com>
2022-02-16 21:38:25 -0300

    Link-logo

--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,1 +1,4 @@
-<img src="https://growwar.s3-sa-east-1.amazonaws.com/growgames/Content/img/novoLayout2/logos/war.png">
+<a href="http://war-grow.herokuapp.com/">
+    <img src="https://growwar.s3-sa-east-1.amazonaws.com/growgames/Content/img/novoLayout2/logos/war.png" alt="WAR-GROW">
+    </a>
+    
