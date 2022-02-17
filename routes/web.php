c4fb585616be76be7b6472d11ed259fec97771bf
Joel Rocha <rochab@gmail.com>
2022-02-17 20:56:21 -0300

    Rota teste

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/teste', function () {
+    return view('teste');
+});
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
