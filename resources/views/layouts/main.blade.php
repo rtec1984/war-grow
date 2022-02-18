73d6f566e992a7a8d511f1f56268cc5214eda4b7
Joel Rocha <rochab@gmail.com>
2022-02-17 21:06:30 -0300

    Teste menu e rodapé

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,18 +1,71 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
   <head>
     <!-- Meta tags Obrigatórias -->
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <title>Olá, mundo!</title>
-  </head>
-  <body>
-    <h1>Olá, mundo!</h1>
-
+    <title>WAR-GROW</title>
+<body>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <div class="collapse navbar-collapse" id="navbar">
+        <a href="/" class="navbar-brand">
+          <img src="https://cdn.awsli.com.br/255/255552/logo/7ac966010f.jpg" alt="RTEC">
+        </a>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a href="/" class="nav-link">EVENTOS</a>
+          </li>
+          <li class="nav-item">
+            <a href="/events/create" class="nav-link">CRIAR EVENTOS</a>
+          </li>
+          @auth
+          <li class="nav-item">
+            <a href="/dashboard" class="nav-link">MEUS EVENTOS</a>
+          </li>
+          <li class="nav-item">
+            <form action="/logout" method="POST">
+              @csrf
+              <a href="/logout" 
+                class="nav-link" 
+                onclick="event.preventDefault();
+                this.closest('form').submit();">
+                SAIR
+              </a>
+            </form>
+          </li>
+          @endauth
+          @guest
+          <li class="nav-item">
+            <a href="/login" class="nav-link">ENTRAR</a>
+          </li>
+          <li class="nav-item">
+            <a href="/register" class="nav-link">CADASTRAR</a>
+          </li>
+          @endguest
+        </ul>
+      </div>
+    </nav>
+  </header>
+  <main>
+    <div class="container-fluid">
+      <div class="row">
+        @if(session('msg'))
+          <p class="msg">{{ session('msg') }}</p>
+        @endif
+        @yield('content')
+      </div>
+    </div>
+  </main>
+  <footer>
+    <div class="rtec">
+    <p>FEITO POR <a href="https://rochatec.lojaintegrada.com.br/" target="_blank" class="rtec">RTEC</a> &copy; 2017-<?php echo date('Y'); ?></p>
+  </footer>
+  <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
     <!-- JavaScript (Opcional) -->
     <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
