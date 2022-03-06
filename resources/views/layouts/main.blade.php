b251c5e042391c0df0482549149f4af7bfecdf30
Joel Rocha <rochab@gmail.com>
2022-03-06 15:55:22 -0300

    Menu e icones

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,46 +14,38 @@
 
 <body>
   <header>
-    <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
-      <!-- Primary Navigation Menu -->
-      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-          <div class="flex">
-            <!-- Logo -->
-            <div class="shrink-0 flex items-center">
-              <x-jet-application-mark class="block h-9 w-auto" />
-            </div>
-          </div>
-          <!-- Navigation Links -->
-          <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-            <x-jet-nav-link href="/ranking">
-              {{ __('Ranking') }}
-            </x-jet-nav-link>
-            <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-              {{ __('Cadastrar Resultado') }}
-            </x-jet-nav-link>
-            @guest
-            <x-jet-nav-link href="/login">
-              {{ __('Entrar') }}
-            </x-jet-nav-link>
-            <x-jet-nav-link href="/register">
-              {{ __('Cadastrar Jogador') }}
-            </x-jet-nav-link>
-            @endguest
-            @auth
-            <form method="POST" action="{{ route('logout') }}">
-              @csrf
-              <x-jet-nav-link href="{{ route('logout') }}" onclick="event.preventDefault();
-                              this.closest('form').submit();">
-                {{ __('Sair') }}
-              </x-jet-nav-link>
-            </form>
-            @endauth
-          </div>
-        </div>
-      </div>
-      </div>
-    </nav>
+<!-- Load an icon library to show a hamburger menu (bars) on small screens -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<div class="topnav" id="myTopnav">
+  <x-jet-application-mark class="block h-9 w-auto" />
+  <x-jet-nav-link href="/ranking"><i class="fa fa-fw fa-trophy"></i>
+    {{ __('Ranking') }}
+  </x-jet-nav-link>
+  <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')"><i class="fa fa-fw fa-plus"></i>
+    {{ __('Cadastrar Resultado') }}
+  </x-jet-nav-link>
+  @guest
+  <x-jet-nav-link href="/login"><i class="fa fa-fw fa-user"></i>
+    {{ __('Entrar') }}
+  </x-jet-nav-link>
+  <x-jet-nav-link href="/register"><i class="fa fa-fw fa-user-plus"></i>
+    {{ __('Cadastrar Jogador') }}
+  </x-jet-nav-link>
+  @endguest
+  @auth
+  <form method="POST" action="{{ route('logout') }}">
+    @csrf
+    <x-jet-nav-link href="{{ route('logout') }}" onclick="event.preventDefault();
+                    this.closest('form').submit();">
+      {{ __('Sair') }}
+    </x-jet-nav-link>
+  </form>
+  @endauth
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
   </header>
   <main>
     <div class="container-fluid">
@@ -69,19 +61,19 @@
     <div class="rtec">
       <ul class="container-fluid">
         <li class="nav-item">
-          <a href="https://rochatec.lojaintegrada.com.br/" target="_blank" class="nav-link">ROCHA TECNOLOGIA &copy;
+          <a href="https://rochatec.lojaintegrada.com.br/" target="_blank" class="nav-link"><i class="fa fa-fw fa-laptop"></i> ROCHA TECNOLOGIA &copy;
             2017-
             <?php echo date('Y'); ?>
           </a>
         </li>
         <li class="nav-item">
-          <a href="https://www.instagram.com/war_grow/" target="_blank" class="nav-link">PERFIL NO INSTAGRAM</a>
+          <a href="https://www.instagram.com/war_grow/" target="_blank" class="nav-link"><i class="fa fa-fw fa-instagram"></i> PERFIL NO INSTAGRAM</a>
         </li>
         <li class="nav-item">
-          <a href="https://discord.com/invite/7cNqtMQpnt" target="_blank" class="nav-link">COMUNIDADE NO DISCORD</a>
+          <a href="https://discord.com/invite/7cNqtMQpnt" target="_blank" class="nav-link"><i class="fa fa-fw fa-discord"></i> COMUNIDADE NO DISCORD</a>
         </li>
         <li class="nav-item">
-          <a href="https://chat.whatsapp.com/Kw5aROV8UboCTtGVPnkhbC" target="_blank" class="nav-link">GRUPO NO WHATSAPP
+          <a href="https://chat.whatsapp.com/Kw5aROV8UboCTtGVPnkhbC" target="_blank" class="nav-link"><i class="fa fa-fw fa-whatsapp"></i> GRUPO NO WHATSAPP
             &copy; 2019-
             <?php echo date('Y'); ?>
           </a>
@@ -89,6 +81,16 @@
       </ul>
   </footer>
   <!-- JavaScript (Opcional) -->
+  <script>
+    function myFunction() {
+      var x = document.getElementById("myTopnav");
+      if (x.className === "topnav") {
+        x.className += " responsive";
+      } else {
+        x.className = "topnav";
+      }
+    }
+    </script>
   <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
     integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
