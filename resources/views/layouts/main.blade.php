180393080e6d98c8ea0325ca540b59d1da3e064a
Joel Rocha <rochab@gmail.com>
2022-03-06 23:22:57 -0300

    Rodapé Novo Icones

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,42 +10,49 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
     integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <link rel="stylesheet" href="/css/app.css">
+  <!-- Font Awesome -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet" />
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+  <!-- MDB -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.css" rel="stylesheet" />
   <title>@yield('title')</title>
 
 <body>
   <header>
-<!-- Load an icon library to show a hamburger menu (bars) on small screens -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Load an icon library to show a hamburger menu (bars) on small screens -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<div class="topnav" id="myTopnav">
-  <x-jet-application-mark class="block h-9 w-auto" />
-  <x-jet-nav-link href="/ranking"><i class="fa fa-fw fa-trophy"></i>
-    {{ __('Ranking') }}
-  </x-jet-nav-link>
-  <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')"><i class="fa fa-fw fa-plus"></i>
-    {{ __('Cadastrar Resultado') }}
-  </x-jet-nav-link>
-  @guest
-  <x-jet-nav-link href="/login"><i class="fa fa-fw fa-user"></i>
-    {{ __('Entrar') }}
-  </x-jet-nav-link>
-  <x-jet-nav-link href="/register"><i class="fa fa-fw fa-user-plus"></i>
-    {{ __('Cadastrar Jogador') }}
-  </x-jet-nav-link>
-  @endguest
-  @auth
-  <form method="POST" action="{{ route('logout') }}">
-    @csrf
-    <x-jet-nav-link href="{{ route('logout') }}" onclick="event.preventDefault();
-                    this.closest('form').submit();">
-      {{ __('Sair') }}
-    </x-jet-nav-link>
-  </form>
-  @endauth
-  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-    <i class="fa fa-bars"></i>
-  </a>
-</div>
+    <div class="topnav" id="myTopnav">
+      <x-jet-application-mark class="block h-9 w-auto" />
+      <x-jet-nav-link href="/ranking"><i class="fa fa-fw fa-trophy"></i>
+        {{ __('Ranking') }}
+      </x-jet-nav-link>
+      <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')"><i
+          class="fa fa-fw fa-plus"></i>
+        {{ __('Cadastrar Resultado') }}
+      </x-jet-nav-link>
+      @guest
+      <x-jet-nav-link href="/login"><i class="fas fa-user-check"></i>
+        {{ __('Entrar') }}
+      </x-jet-nav-link>
+      <x-jet-nav-link href="/register"><i class="fas fa-user-plus"></i>
+        {{ __('Cadastrar Jogador') }}
+      </x-jet-nav-link>
+      @endguest
+      @auth
+      <form method="POST" action="{{ route('logout') }}">
+        @csrf
+        <x-jet-nav-link href="{{ route('logout') }}" onclick="event.preventDefault();
+                    this.closest('form').submit();"><i class="fas fa-user-times"></i>
+          {{ __('Sair') }}
+        </x-jet-nav-link>
+      </form>
+      @endauth
+      <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        <i class="fa fa-bars"></i>
+      </a>
+    </div>
   </header>
   <main>
     <div class="container-fluid">
@@ -57,28 +64,46 @@
       </div>
     </div>
   </main>
-  <footer>
+  <footer class="bg-dark bg-gradient text-center text-white">
+    <!-- Grid container -->
+    <div class="container p-4 pb-0">
+      <!-- Section: Social media -->
+      <section class="mb-4">
+        <!-- Facebook -->
+        <a class="btn btn-primary btn-floating m-1" style="background-color: #3b5998;" href="#!" role="button"><i
+            class="fab fa-facebook-f"></i></a>
+
+        <!-- Twitter -->
+        <a class="btn btn-primary btn-floating m-1" style="background-color: #55acee;" href="#!" role="button"><i
+            class="fab fa-twitter"></i></a>
+
+        <!-- Google -->
+        <a class="btn btn-primary btn-floating m-1" style="background-color: #dd4b39;" href="#!" role="button"><i
+            class="fab fa-google"></i></a>
+
+        <!-- Instagram -->
+        <a class="btn btn-primary btn-floating m-1" style="background-color: #ac2bac;" href="#!" role="button"><i
+            class="fab fa-instagram"></i></a>
+
+        <!-- Linkedin -->
+        <a class="btn btn-primary btn-floating m-1" style="background-color: #0082ca;" href="#!" role="button"><i
+            class="fab fa-linkedin-in"></i></a>
+        <!-- Github -->
+        <a class="btn btn-primary btn-floating m-1" style="background-color: #333333;" href="#!" role="button"><i
+            class="fab fa-github"></i></a>
+      </section>
+      <!-- Section: Social media -->
+    </div>
+
+    <!-- Copyright -->
     <div class="rtec">
-      <ul class="container-fluid">
-        <li class="nav-item">
-          <a href="https://rochatec.lojaintegrada.com.br/" target="_blank" class="nav-link"><i class="fa fa-fw fa-laptop"></i> ROCHA TECNOLOGIA &copy;
-            2017-
-            <?php echo date('Y'); ?>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="https://www.instagram.com/war_grow/" target="_blank" class="nav-link"><i class="fa fa-fw fa-instagram"></i> PERFIL NO INSTAGRAM</a>
-        </li>
-        <li class="nav-item">
-          <a href="https://discord.com/invite/7cNqtMQpnt" target="_blank" class="nav-link"><i class="fa fa-fw fa-discord"></i> COMUNIDADE NO DISCORD</a>
-        </li>
-        <li class="nav-item">
-          <a href="https://chat.whatsapp.com/Kw5aROV8UboCTtGVPnkhbC" target="_blank" class="nav-link"><i class="fa fa-fw fa-whatsapp"></i> GRUPO NO WHATSAPP
-            &copy; 2019-
-            <?php echo date('Y'); ?>
-          </a>
-        </li>
-      </ul>
+      <a href="https://rochatec.lojaintegrada.com.br/" target="_blank" class="nav-link"><i
+          class="fa fa-fw fa-laptop"></i> ROCHA TECNOLOGIA &copy;
+        2017-
+        <?php echo date('Y'); ?>
+      </a>
+    </div>
+    <!-- Copyright -->
   </footer>
   <!-- JavaScript (Opcional) -->
   <script>
@@ -90,7 +115,7 @@
         x.className = "topnav";
       }
     }
-    </script>
+  </script>
   <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
     integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
@@ -102,6 +127,8 @@
     integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
   </script>
   <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
+  <!-- MDB -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.js"></script>
 </body>
 
 </html>
