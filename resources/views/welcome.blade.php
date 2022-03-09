9de2b4da3456a090a087f087efe86803f4dc12c5
Joel Rocha <rochab@gmail.com>
2022-03-08 22:44:45 -0300

    Home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,21 @@
 
 @section('content')
 
-<h1>Regras</h1>   
+
+<div class="wg">
+    <img src="https://war-grow.s3.us-east-1.amazonaws.com/war-grow.png?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEGIaCXNhLWVhc3QtMSJHMEUCIE4ZpG1lQkvVTU5c%2Fwqj%2FVjOETTVYjVL2rvR1NJ3MhTxAiEA4K0f1ljQ2gOEeYYq0b6L7s2%2FfBvjREv5kOF%2FxIcmLJQq7QIIy%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARAAGgwzMzY5NzU4MTEyNTciDAtBXTl3lHhuiTnfQirBAh4znSNbvgA69lwHqICGc%2FMjj7UnCqug%2BtKPGonvvKP0%2F2%2BvZYZmR3W3SG8KexnJYPDXKmTnLxxqgbpzx3ZHlvheh7aKU0ep4NbGIcp4tQIxcYeKiC3oLwEn9ytTwYnVr1oU5dphKj%2BzQ57Y1fU%2BCq%2BuPtjphfhUhoEx4OEoMw5xQlgkf0T4ozxea5wJrkaj3sYL%2B10Yd4Yl3rvNZDM8itSvMyV1%2BqpMcJ%2FrW644nV59R8k9AIKbWWgJ1b9A2dbSImOtfNKFMIEKG%2FQ1PYKofnxcje0QqUC0NRt%2Bacn9dtxiWR9sNvRJRJdQpMxzCK2X7elChDMFaF%2FY0zS2Hx6YZPOWBbhuDkioT%2FL8biNwEfGWgX%2FSsNcxZBXsXpwVM0vDmrlhpOG1V%2Bw5%2BUaxhaFegZSdh6hEyScOXnyyfhaNiLfg4zDKiKCRBjqzAlu1aZIvQSEAGU6Rs7R%2B4vpMuiM%2BMhhq5VWK98%2BFuG2CRWW%2F5E4LYCiFRlvAN%2BAQ8r6JnDrR6t%2Bg4TBNPRL05mTXI%2FMSFJKMr%2F0bVPw1e5NJ2Wxd%2FW%2B10agwKUV6An43NBK6HcOA2r7MWOeM2Yr6sy7jb9wO0oXKiRhw%2FsxPSh%2FwdF56YhqHO5Fmy39r7CYFlXyFc%2FdOnoe7EUhUgrX8lFNZnhLVwF583IiOdpV8nPmNm7cvpXlTi80YmUBVVLieonyl47QKYor%2BlvDSqkAcCsCBB9n8BlwAUh%2FNMJ9WfrwOuOAudMb36URZqBzBtdbt1%2F7i4sFxZu02PLetsZSR%2BqlGNVNaGtr50D%2BXDT9lXaZNg%2B%2BwHg0A98ugUsxwoYNVm6AP%2B%2B8YkYrJDrOu1jlWLBt15bs%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20220309T014322Z&X-Amz-SignedHeaders=host&X-Amz-Expires=299&X-Amz-Credential=ASIAU45KS2K463S73QMT%2F20220309%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=154b21941cc94446237f07b28359969198409dc8e10773271a78074870f71572" class="img-thumbnail" />
+
+    <p>Jogar limpo é a melhor estratégia!</p>
+
+    <p>Temos uma regra importante: jogar limpo, sempre.</p>
+    
+    <p>Agimos com honestidade, respeito e transparência!</p>
+    
+    <p>As estratégias podem mudar, mas jogar limpo é condição.</p>
+    
+    <p>Sorte é para quem merece!</p>
+    
+    <p>Tudo o que fazemos é pensado para oferecer desafios na medida certa, com equilíbrio entre sorte e estratégia.</p>
+</div>   
 
 @endsection
