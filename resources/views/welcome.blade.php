85d75bbe2e854f739476003a484e0c3e6421815a
Joel Rocha <rochab@gmail.com>
2022-03-09 23:28:12 -0300

    footer relative

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,21 +4,21 @@
 
 @section('content')
 
-<div class="container"
 <div class="wg">
     <img src="https://war-grow.s3.amazonaws.com/war-grow.png" class="img-thumbnail" />
 
-    <p>Jogar limpo é a melhor estratégia!</p>
+    <p class="wg">Jogar limpo é a melhor estratégia!</p>
 
-    <p>Temos uma regra importante: jogar limpo, sempre.</p>
-    
-    <p>Agimos com honestidade, respeito e transparência!</p>
-    
-    <p>As estratégias podem mudar, mas jogar limpo é condição.</p>
-    
-    <p>Sorte é para quem merece!</p>
-    
-    <p>Tudo o que fazemos é pensado para oferecer desafios na medida certa, com equilíbrio entre sorte e estratégia.</p>
-</div>   
+    <p class="wg">Temos uma regra importante: jogar limpo, sempre.</p>
+
+    <p class="wg">Agimos com honestidade, respeito e transparência!</p>
+
+    <p class="wg">As estratégias podem mudar, mas jogar limpo é condição.</p>
+
+    <p class="wg">Sorte é para quem merece!</p>
+
+    <p class="wg">Tudo o que fazemos é pensado para oferecer desafios na medida certa, com equilíbrio entre sorte e
+        estratégia.</p>
 </div>
+
 @endsection
