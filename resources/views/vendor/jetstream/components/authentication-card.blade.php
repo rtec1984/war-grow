8eafcf15401283e8359e438d9d23c73557308647
Joel Rocha <rochab@gmail.com>
2022-05-29 02:53:24 -0300

    auth-card

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -3,7 +3,12 @@
         {{ $logo }}
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg" style="
+    padding-top: 1.5rem;
+    margin-left: auto;
+    margin-right: auto;
+    border-width: 0.5rem;
+">
         {{ $slot }}
     </div>
 </div>
