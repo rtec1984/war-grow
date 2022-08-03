ba612a87f56cd1cf5a610d9e459ffc60505f8831
Joel Rocha <rochab@gmail.com>
2022-08-03 19:58:28 -0300

    Historic

--- resources/views/historic.blade.php
+++ resources/views/historic.blade.php
@@ -8,8 +8,104 @@
     <div class="row">
         <div class="col-md-6">
             <br>
+            {{-- <h4>Ranking Final de Agosto de 2022</h4>
+            <table class="table table-bordered table-striped table-hover table-light table-responsive" id="agosto22">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">NOME</th>
+                        <th scope="col">P</th>
+                        <th scope="col">J</th>
+                        <th scope="col">V</th>
+                        <th scope="col">%</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scropt="row">1</td>
+                        <td>Levi Francalino</td>
+                        <td>36</td>
+                        <td>7</td>
+                        <td>5</td>
+                        <td>71</td>
+                    </tr>
+                    <tr>
+                        <td scropt="row">2</td>
+                        <td>Paulo Santos</td>
+                        <td>27</td>
+                        <td>22</td>
+                        <td>8</td>
+                        <td>36</td>
+                    </tr>
+                    <tr>
+                        <td scropt="row">3</td>
+                        <td>Joel Rocha</td>
+                        <td>24</td>
+                        <td>27</td>
+                        <td>8</td>
+                        <td>30</td>
+                    </tr>
+                    <tr>
+                        <td scropt="row">4</td>
+                        <td>Kabeloooo</td>
+                        <td>20</td>
+                        <td>8</td>
+                        <td>4</td>
+                        <td>50</td>
+                    </tr>
+                    <tr>
+                        <td scropt="row">5</td>
+                        <td>Saulo Chevitarese</td>
+                        <td>12</td>
+                        <td>13</td>
+                        <td>4</td>
+                        <td>31</td>
+                    </tr>
+                    <tr>
+                        <td scropt="row">6</td>
+                        <td>Jessica</td>
+                        <td>6</td>
+                        <td>14</td>
+                        <td>3</td>
+                        <td>21</td>
+                    </tr>
+                    <tr>
+                        <td scropt="row">7</td>
+                        <td>SINCERAGE</td>
+                        <td>4</td>
+                        <td>11</td>
+                        <td>2</td>
+                        <td>18</td>
+                    </tr>
+                    <tr>
+                        <td scropt="row">8</td>
+                        <td>Frederico Lima</td>
+                        <td>3</td>
+                        <td>3</td>
+                        <td>1</td>
+                        <td>33</td>
+                    </tr>
+                    <tr>
+                        <td scropt="row">9</td>
+                        <td>Junior Barros</td>
+                        <td>3</td>
+                        <td>4</td>
+                        <td>1</td>
+                        <td>25</td>
+                    </tr>
+                    <tr>
+                        <td scropt="row">10</td>
+                        <td>RENATO KINAIP</td>
+                        <td>3</td>
+                        <td>4</td>
+                        <td>1</td>
+                        <td>25</td>
+                    </tr>
+                </tbody>
+            </table>
+            <br> --}}
             <h4>Ranking Final de Julho de 2022</h4>
-            <table class="table table-bordered table-striped table-hover table-light table-responsive" id="ranking">
+            <table class="table table-bordered table-striped table-hover table-light table-responsive" id="julho22">
                 <thead>
                     <tr>
                         <th scope="col">#</th>
@@ -110,4 +206,34 @@
         </div>
     </div>
 </div>
+<!-- DataTables -->
+<script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="//cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
+<script>
+    $(document).ready(function() {
+    $('#julho22').DataTable({
+
+        "language": {
+          "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Portuguese-Brasil.json"
+        },
+
+        paging: false,
+        info: false,
+    });
+});
+</script>
+{{-- <script>
+    $(document).ready(function() {
+      $('#agosto22').DataTable({
+  
+          "language": {
+            "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Portuguese-Brasil.json"
+          },
+  
+          paging: false,
+          info: false,
+  
+      });
+  });
+</script> --}}
 @endsection
