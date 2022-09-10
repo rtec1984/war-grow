ebbab2bfbf33a42b61e50029aa52ac82b16ba2b7
Joel Rocha <rochab@gmail.com>
2022-09-10 16:29:49 -0300

    Histórico

--- resources/views/historic.blade.php
+++ resources/views/historic.blade.php
@@ -8,6 +8,102 @@
     <div class="row">
         <div class="col-md-6">
             <br>
+            <h4>Ranking Final de Agosto de 2022</h4>
+            <table class="table table-bordered table-striped table-hover table-light table-responsive" id="agosto22">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">NOME</th>
+                        <th scope="col">P</th>
+                        <th scope="col">J</th>
+                        <th scope="col">V</th>
+                        <th scope="col">%</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scropt="row">1</td>
+                        <td>Thiago Gasparino</td>
+                        <td>65</td>
+                        <td>7</td>
+                        <td>7</td>
+                        <td>100</td>
+                    </tr>
+                    <tr>
+                        <td scropt="row">2</td>
+                        <td>Andressa Lopes</td>
+                        <td>48</td>
+                        <td>59</td>
+                        <td>17</td>
+                        <td>29</td>
+                    </tr>
+                    <tr>
+                        <td scropt="row">3</td>
+                        <td>Eloa Alonso</td>
+                        <td>39</td>
+                        <td>74</td>
+                        <td>18</td>
+                        <td>24</td>
+                    </tr>
+                    <tr>
+                        <td scropt="row">4</td>
+                        <td>Elson Alves</td>
+                        <td>38</td>
+                        <td>25</td>
+                        <td>10</td>
+                        <td>40</td>
+                    </tr>
+                    <tr>
+                        <td scropt="row">5</td>
+                        <td>Joel Rocha</td>
+                        <td>35</td>
+                        <td>39</td>
+                        <td>12</td>
+                        <td>31</td>
+                    </tr>
+                    <tr>
+                        <td scropt="row">6</td>
+                        <td>Paulo Santos</td>
+                        <td>16</td>
+                        <td>30</td>
+                        <td>7</td>
+                        <td>23</td>
+                    </tr>
+                    <tr>
+                        <td scropt="row">7</td>
+                        <td>Henrique Manoel</td>
+                        <td>13</td>
+                        <td>3</td>
+                        <td>2</td>
+                        <td>67</td>
+                    </tr>
+                    <tr>
+                        <td scropt="row">8</td>
+                        <td>Saulo Chevitarese</td>
+                        <td>13</td>
+                        <td>6</td>
+                        <td>3</td>
+                        <td>50</td>
+                    </tr>
+                    <tr>
+                        <td scropt="row">9</td>
+                        <td>Renato Kinaip</td>
+                        <td>12</td>
+                        <td>27</td>
+                        <td>6</td>
+                        <td>22</td>
+                    </tr>
+                    <tr>
+                        <td scropt="row">10</td>
+                        <td>Walter Luis</td>
+                        <td>10</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>100</td>
+                    </tr>
+                </tbody>
+            </table>
+            <br>
             <h4>Ranking Final de Julho de 2022</h4>
             <table class="table table-bordered table-striped table-hover table-light table-responsive" id="julho22">
                 <thead>
@@ -47,7 +143,7 @@
                     </tr>
                     <tr>
                         <td scropt="row">4</td>
-                        <td>Kabeloooo</td>
+                        <td>Wanderson Roza</td>
                         <td>20</td>
                         <td>8</td>
                         <td>4</td>
@@ -63,7 +159,7 @@
                     </tr>
                     <tr>
                         <td scropt="row">6</td>
-                        <td>Jessica</td>
+                        <td>Jessica Barroaa</td>
                         <td>6</td>
                         <td>14</td>
                         <td>3</td>
@@ -71,7 +167,7 @@
                     </tr>
                     <tr>
                         <td scropt="row">7</td>
-                        <td>SINCERAGE</td>
+                        <td>Marciano Conrado</td>
                         <td>4</td>
                         <td>11</td>
                         <td>2</td>
@@ -95,7 +191,7 @@
                     </tr>
                     <tr>
                         <td scropt="row">10</td>
-                        <td>RENATO KINAIP</td>
+                        <td>Renato Kinaip</td>
                         <td>3</td>
                         <td>4</td>
                         <td>1</td>
@@ -104,7 +200,103 @@
                 </tbody>
             </table>
             <br>
-            <h4>Ranking Geral de Outubro de 2019 à Dezembro de 2021</h4>
+            <h4>Ranking de Janeiro de 2022 à Maio de 2022</h4>
+            <table class="table table-bordered table-striped table-hover table-light table-responsive" id="geral2">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">NOME</th>
+                        <th scope="col">P</th>
+                        <th scope="col">J</th>
+                        <th scope="col">V</th>
+                        <th scope="col">%</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scropt="row">1</td>
+                        <td>Paulo Santos</td>
+                        <td>232</td>
+                        <td>146</td>
+                        <td>53</td>
+                        <td>36</td>
+                    </tr>
+                    <tr>
+                        <td scropt="row">2</td>
+                        <td>Bárbara Moreira</td>
+                        <td>49</td>
+                        <td>58</td>
+                        <td>18</td>
+                        <td>31</td>
+                    </tr>
+                    <tr>
+                        <td scropt="row">3</td>
+                        <td>Diego Alves</td>
+                        <td>35</td>
+                        <td>6</td>
+                        <td>4</td>
+                        <td>67</td>
+                    </tr>
+                    <tr>
+                        <td scropt="row">4</td>
+                        <td>Rondinei Silva</td>
+                        <td>34</td>
+                        <td>41</td>
+                        <td>11</td>
+                        <td>27</td>
+                    </tr>
+                    <tr>
+                        <td scropt="row">5</td>
+                        <td>Luís Felipe</td>
+                        <td>33</td>
+                        <td>29</td>
+                        <td>9</td>
+                        <td>31</td>
+                    </tr>
+                    <tr>
+                        <td scropt="row">6</td>
+                        <td>Natália Albedo</td>
+                        <td>32</td>
+                        <td>4</td>
+                        <td>3</td>
+                        <td>75</td>
+                    </tr>
+                    <tr>
+                        <td scropt="row">7</td>
+                        <td>Ana Formiga</td>
+                        <td>32</td>
+                        <td>55</td>
+                        <td>11</td>
+                        <td>20</td>
+                    </tr>
+                    <tr>
+                        <td scropt="row">8</td>
+                        <td>Augusto Pedrosa</td>
+                        <td>31</td>
+                        <td>21</td>
+                        <td>8</td>
+                        <td>38</td>
+                    </tr>
+                    <tr>
+                        <td scropt="row">9</td>
+                        <td>Joel Rocha</td>
+                        <td>29</td>
+                        <td>56</td>
+                        <td>12</td>
+                        <td>21</td>
+                    </tr>
+                    <tr>
+                        <td scropt="row">10</td>
+                        <td>Henrique Castro</td>
+                        <td>19</td>
+                        <td>3</td>
+                        <td>2</td>
+                        <td>67</td>
+                    </tr>
+                </tbody>
+            </table>
+            <br>
+            <h4>Ranking de Outubro de 2019 à Dezembro de 2021</h4>
             <table class="table table-bordered table-striped table-hover table-light table-responsive" id="geral">
                 <thead>
                     <tr>
@@ -135,7 +327,7 @@
                     </tr>
                     <tr>
                         <td scropt="row">3</td>
-                        <td>Barbara Moreira</td>
+                        <td>Bárbara Moreira</td>
                         <td>1008</td>
                         <td>675</td>
                         <td>204</td>
@@ -175,7 +367,7 @@
                     </tr>
                     <tr>
                         <td scropt="row">8</td>
-                        <td>Di Alvez</td>
+                        <td>Diego Alves</td>
                         <td>612</td>
                         <td>375</td>
                         <td>117</td>
@@ -211,7 +403,33 @@
 <script src="//cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
 <script>
     $(document).ready(function() {
+    $('#agosto22').DataTable({
+
+        "language": {
+          "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Portuguese-Brasil.json"
+        },
+
+        paging: false,
+        info: false,
+    });
+});
+</script>
+<script>
+    $(document).ready(function() {
     $('#julho22').DataTable({
+
+        "language": {
+          "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Portuguese-Brasil.json"
+        },
+
+        paging: false,
+        info: false,
+    });
+});
+</script>
+<script>
+    $(document).ready(function() {
+    $('#geral2').DataTable({
 
         "language": {
           "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Portuguese-Brasil.json"
