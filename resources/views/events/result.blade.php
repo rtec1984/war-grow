b8cf8a7de82b297b8234bc4cb87da8dde2309b3a
Joel Rocha <rochab@gmail.com>
2022-09-24 18:21:44 -0300

    SortByName

--- resources/views/events/result.blade.php
+++ resources/views/events/result.blade.php
@@ -11,7 +11,7 @@
       <p class="card-game"><ion-icon name="calendar"></ion-icon>&nbsp; {{ date('d/m/Y', strtotime($event->date)) }}</p>
       <p class="card-game"><ion-icon name="trophy"></ion-icon>&nbsp; {{ $eventOwner['name'] }}</p>
       {{-- <p class="card-game"><ion-icon name="people"></ion-icon>&nbsp; {{ count($event->users) }} Confirmados</p> --}}
-      @foreach($event->users as $name)
+      @foreach($event->users->sortBy(['name', 'asc']) as $name)
       <p class="card-game"><ion-icon name="person"></ion-icon>&nbsp; {{ $name->name }}</p>
       @endforeach
         @if(count($event->users)<($event->players)) @if(!$hasUserJoined) <form action="/events/join/{{ $event->id }}" id="formJoin" method="POST" style="display: flex; justify-content: center;">
