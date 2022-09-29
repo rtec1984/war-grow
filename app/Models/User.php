97f0eae7b606d81ab49f9e2f7a1e2870eb27e5bc
Joel Rocha <rochab@gmail.com>
2022-09-29 15:35:29 -0300

    UserCrud

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+    use \Backpack\CRUD\app\Models\Traits\CrudTrait;
     use HasApiTokens;
     use HasFactory;
     use HasProfilePhoto;
